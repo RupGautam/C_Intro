64a6498fa6358f00b0a92daf3b341480405cca21
Rup Gautam <rup.gautam@yahoo.com>
2016-05-25 23:49:25 -0400

    touched file name

---
+++ w3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+
+    // variable defining player's stats
+    const int pStrength = 10;
+    const int pDefense = 20;
+    const int pIntelligence = 40;
+    const int pHP = 10;
+    // varible defining enemy's stats
+    const int eStrength = 30;
+    const int eDefense = 40;
+    const int eIntelligence = 25;
+    const int eHP = 10;
+
+
+   //starting game
+   printf("\n\n\n");
+   printf("+------------------------------------+\n");
+   printf("|------------------------------------|\n");
+   printf("|   B A T T L E    S T A R T S ! ! ! |\n");
+   printf("|------------------------------------|\n");
+   printf("+------------------------------------+\n");
+
+   printf("    Your HP:   %d Enemy HP:   %d\n\n\n", pHP, eHP);
+
+
+
+return 0;
+
+    }
