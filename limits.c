4dfe8724d0a0814094e9ed0a49e3cbdac825425a
Rup Gautam <rup.gautam@yahoo.com>
2016-05-28 00:40:57 -0400

    working

---
+++ limits.c
@@ -0,0 +1,26 @@
+// Limits on Arithmetic Expressions
+//
+// limits.c
+// Rup Gautam 
+
+
+  int main(void)
+    {
+                int i, j, ij;
+                double x, y, xy;
+
+
+                printf("Enter first number: \n");
+                scanf("%d", &i);
+                printf("Enter second number: \n");
+                scanf("%d", &j);
+                printf("print decimal number: \n");
+                scanf("%lf", &x);
+                printf("print second dec number: \n");
+                scanf("%lf", &y);
+                printf("Total: %d\n", ij);
+                printf("Total dec: %lf\n", xy);
+
+
+                return 0;
+                }
