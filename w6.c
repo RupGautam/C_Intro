1cdfc7b8863e504f5c53171d6f2517eb09d12d96
Rup Gautam <RupGautam@users.noreply.github.com>
2016-07-18 23:14:55 -0400

    working stage w6

--- w6.c
+++ w6.c
@@ -7,3 +7,112 @@
 //  No part of this assignment has been copied manually or electronically from anyother source (including web sites)
 //  or distributed to other students.
 //  Name   Rup Gautam   Student ID  033838152
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+void SeedRandom();
+float GetRandom(float low, float high);
+int validate(int low, int high);
+void ClearInputBuffer();
+
+int main() {
+    int input;
+    do {
+
+        SeedRandom();
+        float randNum;
+
+        randNum = GetRandom(0.15, 0.50);
+        float strenghtRatio = randNum;
+
+        randNum = GetRandom(0.15, 0.50);
+        float speedRatio = randNum;
+
+        randNum = GetRandom(0.15, 0.50);
+        float defenceRatio = randNum;
+
+        randNum = GetRandom(0.15, 0.50);
+        float intelligenceRatio = randNum;
+
+            /* Above block generates random ratio values for: 
+         Strength, Speed, Defense & Intelligence
+         between 0.15-0.5 */
+
+        randNum = GetRandom(0.5, 2.0);
+        float HPratio = randNum;
+
+        randNum = GetRandom(3, 20);
+        float level = randNum;
+
+      	/* Above block generates random ratio values for: 
+         Health Power and Level between 0.5-2.0 */
+
+
+        strenghtRatio = truncf(strenghtRatio * level * level);
+        // math strength x twice level value 
+
+        speedRatio = truncf(speedRatio * level * level);
+        // math strength x twice level value
+
+        defenceRatio = truncf(defenceRatio * level * level);
+        // math strength x twice level value
+
+        intelligenceRatio = truncf(intelligenceRatio * level * level);
+        // math strength x twice level value
+
+        HPratio = truncf(HPratio * level);
+        level = truncf(level);
+
+        /* Above block generates random ratio values for: 
+         Health Power and Level between 0.5-2.0 */
+
+        printf("Level        - %d\n", (int) level);
+        printf("HP           - %d\n", (int) HPratio);
+        printf("Strenght     - %d\n", (int) strenghtRatio);
+        printf("Speed        - %d\n", (int) speedRatio);
+        printf("Defence      - %d\n", (int) defenceRatio);
+        printf("Intelligence - %d\n", (int) intelligenceRatio);
+        printf("Generate Another?  (1 - YES 2 - No) : ");
+        input = validate(1, 2);
+    } while (input != 2);
+
+
+}
+void SeedRandom() 
+{
+srand(time(NULL)); //from lecture 9 week. 
+
+}
+float GetRandom(float low, float high) {
+float x = low + ((float) rand() / RAND_MAX * (high - low));
+// 
+
+return x;
+
+}
+
+
+void ClearInputBuffer() {
+    while (getchar() != '\n');
+}
+
+int validate(int low, int high) {
+    int number, Result;
+    do {
+        scanf("%d", & number);
+
+        if (number >= low && number <= high)
+
+        {
+            Result = number;
+        } else {
+            ClearInputBuffer();
+            printf("Invalid input, try again:  \n");
+        }
+    }
+    while (Result != number);
+    return number;
+}
